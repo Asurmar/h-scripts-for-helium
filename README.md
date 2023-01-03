4cc7f5183d586b5cba3fc3d467b617fda0fe4442
Asurmar <gwarek@zohomail.eu>
2023-01-03 19:05:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Hscripts for Helium hotspot
+[1. Short intro](#1-short-intro)  
+[2. Requirements](#2-requirements)  
+[3. Installation](#3-installation)  
+[4. Updating](#4-updating)  
+[5. Compatibility](#5-compatibility)  
+[6. FAQ](#6-faq)  
+[7. Other stuff](#7-other-stuff)  
+
+### 1. Short intro
+Collection of handy shell scripts for day to day management of a Helium hotspot. Tired of rebooting your hotspot every time it hangs for few hours? Say NO MORE! You can use these scripts to:
+- display miner app process information
+- restart your miner app,
+- restart the entire miner container,
+- safely reboot your hotspot,
+- display connections to validators
+- display statistics and details of witnesses
+- find your helium logs directory
+- display other useful hotspot information (miner version, name, temperature, uptime, public IP)
+
+### 2. Requirements
+1. Access to your hotspot via ssh.
+2. Basic knowledge of CLI (I mean really basic)
+
+### 3. Installation
+Download install script and run it, just copy and paste below commands (one line):  
+<sub>`wget -O install.sh https://github.com/Asurmar/hscripts-for-helium/raw/master/install.sh; chmod +x install.sh; ./install.sh`</sub>  
+On balena-based hotspots (Sensecap, Nebra, Nebra for 3rd party hotspots) first change directory to:  
+`cd /mnt/data/`
+
+### 4. Updating 
+It's enough to just run previously downloaded install script:  
+`./install.sh` : for non-balena hotspots  
+`/mnt/data/install.sh` : for balena-based hotspots (Sensecap, Nebra)  
+
+### 5. Compatibility
+Hscripts work with every Helium hotspot brand that allows ssh access by default:
+- PantherX (at least X1)
+- Milesight
+- Pisces (not tested though)
+- Dusun (not tested)
+
+They also work perfectly with balena-based hotspots, but it requires SD-card modification, which is very easy to do:
+- Sensecap
+- Nebra
+
+Below listed brands can also turn into balena-based hotspots by using Nebra software for 3rd party hotspots:
+- RAK/MNTD (YES!)  
+- Cotx
+- Controllino
+- Finestra
+- Helium OG
+- Syncrobit
+- Linxdot
+- RisingHF
+- Pycom
+
+Other brands that you can tinker with to enable ssh, but it requires knowledge of hardware hacking, UART, serial console and stuff:
+- Heltec
+- Browan
+- Midas
+- Bobcat (?)
+
+### 6. FAQ
+**Q**: What the heck is ssh?  
+**A**: Ssh is a secure shell protocol. It uses encryption to connect to a device's CLI (command line interface - the way geeks talk to computers, you know)
+
+**Q**: Why do I need ssh?  
+**A**: Because you want to be a geek. Just kidding. Ssh gives you the ability to get into the operating system of a hotspot, which allows you to retrieve detailed information (usually not available through other means), browse logs and do other fun staff with your device like automating, writing your own scripts, creating services, etc.
+
+**Q**: How do these scripts work?  
+**A**: They run shell commands to allow you interact with the software and provide valuable information mainly by using 3 different ways: browsing miner logs, talking to the miner app container and querying Helium API.
+
+**Q**: What is Balena-based hotspot?  
+**A**: balenaOS is an operating system designed to run docker containers and used specifically for IoT devices. balenaOS-powered hotspots include Sensecap, Nebra and Nebra for 3rd party hotspots.
+
+**Q**: What is Nebra for 3rd party hotspots?  
+**A**: It a software provided by Nebra for hotspots based on Raspberry Pi Compute Module 4 (CM4) or Rock Compute Module 3, which include brands like: **RAK/MNTD, Cotx, Pisces, Syncrobit, Controllino, Finestra, Helium OG, Linxdot, RisingHF, Pycom**. In short: flash an SD card with Nebra software, insert it into your hotspot and turn it into a balenaOS-powered device with possibility to enable ssh (YES!). It works exactly as before, even the Explorer displays hardware manufacturer correctly.
+
+### 7. Other stuff
+So you CAN actually ssh into a Helium hotspot, now what? How about creating a centralized web dashboard, similar to that provided by Sensecap (as far as I know best Helium dashboard in business), but maybe better? How about adding more services to the dashboard like monitoring network availability, monitoring PoC activity, automating serivce restarts in case of flatlining, environment alerts? Reverse shell server? Looking for people with at least basic experience in creating Python web based aplications to participate in that project. No Helium knowledge required.
---
+++ install.sh
@@ -0,0 +1,122 @@
+#
+# Repository: https://github.com/Asurmar/hscripts-for-helium
+#
+# Description: This script installs hscripts
+#
+# Compatibility:
+# Milesight     +
+# Panther       +
+# Balena-based:
+#   Sensecap    
+#   Nebra       +
+
+debug=false                       # set this to "true" for additional debugging
+targetdir=/usr/local/sbin
+targetdirfull=$targetdir
+#targetdircopyto=$targetdir
+giturl=https://github.com/Asurmar/hscripts-for-helium/raw/master/
+gitfile=hscripts.tar.gz
+rootmode=
+overlaymode=none
+lowerdir=
+shellprofile=
+shellprofilepath=
+remount=false
+
+[ $debug = true ] && echo Debug: Target dir = $targetdir
+
+rootmode=$(mount|grep " / "|awk '{print $6}'|awk -F "," '{print $1}'|sed "s/(//")
+[ $debug = true ] && echo Debug: Root filesystem mode: $rootmode
+if [ $rootmode = ro ]; then
+  [ $debug = true ] && echo Debug: Read-only filesystem, detecting overlay lowerdir.
+  remount=true
+  overlaymode=$(mount|grep -i 'overlay on / type overlay'|awk '{print $6}'|awk -F "," '{print $1}'|sed 's/(//')
+  [ $debug = true ] && echo Debug: Overlay mode = $overlaymode
+  if [ -x "$(command -v balena)" ]; then lowerdir=/mnt/sysroot/active$(mount|egrep -o 'lowerdir=[^, ]*'|sed 's/lowerdir=//')
+                                    else echo Unknown containerization engine, exiting; fi
+  [ $debug = true ] && echo Debug: Overlay lowerdir = $lowerdir
+  [ $debug = true ] && echo Debug: Remount = $remount
+  targetdirfull=$lowerdir$targetdir
+  targetdircopyto=$targetdirfull
+  [ $debug = true ] && echo Debug: Full target dir = $targetdirfull
+  [ $debug = true ] && echo Debug: Target dir copy to = $targetdirfull
+  mount -o remount /
+fi
+
+### Target directory creation
+if [ ! -d $targetdir ]; then
+  [ $debug = true ] && echo Debug: Directory $targetdir does not exist
+  mkdir -p $targetdirfull && { echo Creating directory: $targetdir; echo; }
+  [ $remount = true ] && mount -o remount /
+fi
+
+### Downloading file from github
+[ $debug = true ] && echo Debug: Overlay mode = $overlaymode
+if [ $overlaymode = ro ]; then
+  cd /mnt/data
+  [ $debug = true ] && echo Debug: Current dir: $(pwd)
+fi
+[ $debug = true ] && echo
+echo Downloading from github repository https://github.com/Asurmar/hscripts-for-helium:
+wget -O $gitfile $giturl$gitfile || { echo Failed to download $giturl$gitfile, exiting; exit 6; }
+echo
+
+### Extracting and installing files
+echo Installing in directory: $targetdir
+files=$(tar -tzf $gitfile)
+[ $debug = true ] && echo Debug: List of files: $files
+for file in $files; do
+  [ $debug = true ] && echo Debug: Current file: $file
+  tar -xzvf $gitfile $file > /dev/null || echo Error extracting file: $file
+  mv $file $targetdirfull
+  [ -f $targetdir/$file ] && echo File $file installed successfully.
+done
+rm $gitfile
+echo Installation complete.
+[ $remount = true ] && mount -o remount /
+[ $debug = true ] && ls -l $targetdir 
+echo
+
+### Verifying PATH variable
+[ $debug = true ] && echo Debug: PATH variable: $PATH
+echo $PATH|egrep -o "^$targetdir:|:$targetdir:|:$targetdir$" >/dev/null
+inpath=$?
+[ $debug = true ] && echo Debug: inpath = $inpath
+if [ $inpath -eq 0 ]; then
+  [ $debug = true ] && echo Debug: $targetdir is in PATH, nothing to do, exiting.
+  exit 0
+fi
+
+###
+### If targetdir is not in PATH
+###
+
+### Shell detection
+[ $debug = true ] && echo Debug: Your shell is $SHELL
+case $SHELL in
+  /bin/sh)   shellprofile=.profile;;
+  /bin/ash)  shellprofile=.profile;;
+  /bin/dash) shellprofile=.profile;;
+  /bin/bash) shellprofile=.bashrc;;
+  *)         echo Unknown shell, exiting; exit 6;;
+esac
+[ $debug = true ] && echo Debug: Shell profile is $shellprofile
+
+### Shell profile path
+if [ $rootmode = ro ]; then
+  shellprofilepath=$lowerdir$HOME/$shellprofile
+  [ $remount = true ] && mount -o remount /
+elif [ $rootmode = rw ]; then
+  shellprofilepath=$HOME/$shellprofile
+fi
+[ $debug = true ] && echo Debug: Full path to shell profile file: $shellprofilepath
+
+if [ ! -f $shellprofilepath ]; then
+  [ $debug = true ] && echo Debug: Shell profile file: $shellprofile does NOT exist
+  touch $shellprofilepath && echo Shell profile created: $HOME/$shellprofile
+  [ $remount = true ] && mount -o remount /                               
+fi
+echo "export PATH=\$PATH:$targetdir" >> $shellprofilepath && echo Added $targetdir to \$PATH variable, for scripts to work you MUST do one of 2 things:
+echo "      1) logoff and logon again"
+echo "      2) run this command:"
+echo "            export PATH=\$PATH:$targetdir"
